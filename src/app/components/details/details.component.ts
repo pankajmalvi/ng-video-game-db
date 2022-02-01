25a7d1d0a267b79f016a568c49beb5e7ec49be52
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-02-01 13:29:54 +0530

    Changed the details component static data to the data from the API(dynamic)

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models';
@@ -9,7 +9,7 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
   gameId!: string;
   game!: Game;
@@ -49,6 +49,16 @@ export class DetailsComponent implements OnInit {
       return '#f7aa38';
     } else {
       return '#eff46555';
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
     }
   }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -25,12 +25,12 @@ export class HttpService {
   }
 
   getGameDetails(id: string): Observable<Game> {
-    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games${id}`);
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
     const gameTrailersRequest = this.http.get(
-      `${env.BASE_URL}/games${id}/movies`
+      `${env.BASE_URL}/games/${id}/movies`
     );
     const gameScreeshotsRequest = this.http.get(
-      `${env.BASE_URL}/games${id}/screenshots`
+      `${env.BASE_URL}/games/${id}/screenshots`
     );
 
     return forkJoin({
